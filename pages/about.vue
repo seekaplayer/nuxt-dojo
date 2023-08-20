ce8fa2dfb28657792a10a4810c5ec42e9bde61a5
Seekaplayer <admin@seekaplayer.com>
2023-08-20 19:44:27 -0400

    First Commit

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. corrupti eos
+      adipisci nam repellat possimus aperiam fugiat non? Laudantium, reiciendis
+      repudiandae.
+    </p>
+    <p>
+      Temporibus qui libero exercitationem repellendus, doloremque sit
+      necessitatibus ab at
+    </p>
+    <div>
+      {{ data }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const { data } = await useFetch("/api/currency/USD");
+  console.log(data);
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div v-for="p in products">
+        <ProductCard :product="p" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: "products",
+  });
+
+  // fetch the products
+  const { data: products } = await useFetch(
+    "https://fakestoreapi.com/products"
+  );
+
+  useHead({
+    title: "Nuxt Dojo | Merch",
+    meta: [{ name: "description", content: "Nuxt 3 Merch" }],
+  });
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
